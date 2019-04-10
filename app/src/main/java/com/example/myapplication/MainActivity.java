6c33f469d2ddbca86eb35823620b5f560602152b
Ohyoonki <gkdldn233gmail.com>
2019-04-10 16:45:59 +0900

    다음주 할것. 1. 팝업에 알람 2. 디자인 3. 가로로했을때

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends Activity {
             }
 
     };
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
