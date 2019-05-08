a88057470279f9570896e5677873d0b955cfafec
cjoisunghoo <chlsh0681@gmail.com>
2019-05-08 16:19:09 +0900

    메뉴 옵션바 색상 변경 및 버튼 정리

--- app/src/main/java/com/example/myapplication/IntroActivity.java
+++ app/src/main/java/com/example/myapplication/IntroActivity.java
@@ -1,13 +1,14 @@
 package com.example.myapplication;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends Activity {
     Handler handler;
 
     Runnable runnable = new Runnable() {
